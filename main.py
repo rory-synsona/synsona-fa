8461a9df43a4f12081760ff9272b5744dc991832
rory-synsona <rory@synsona.com>
2025-03-22 22:11:56 -0400

    Added get as well

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+'''
 
 @app.get("/echo/")
 async def echo(value: str = Query(..., description="The value to be echoed")):
     return {"confirmation61": value}
-'''
 
 class EchoRequest(BaseModel):
     value: str
 
 @app.post("/echo/")
 async def echo(request: EchoRequest):
-    return {"confirmation": request.value}
+    return {"confirmation93": request.value}
