e58bcb00a89d8400022295c442c45f0c13a440cf
rory-synsona <rory@synsona.com>
2025-03-22 21:52:49 -0400

    Test with post request

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
+'''
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World2!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/echo/")
+async def echo(value: str = Query(..., description="The value to be echoed")):
+    return {"confirmation61": value}
+'''
+
+class EchoRequest(BaseModel):
+    value: str
+
+@app.post("/echo/")
+async def echo(request: EchoRequest):
+    return {"confirmation": request.value}
