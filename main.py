45057dab700a2f97f5a6c96157fd88970219e608
rory-synsona <rory@synsona.com>
2025-03-22 21:59:50 -0400

    Added CORS

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 '''
 @app.get("/")
